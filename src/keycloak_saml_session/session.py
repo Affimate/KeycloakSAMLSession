99ebb0ac0a9f164ce0c5fb2d47178387c272eb6b
Mi4BT <tb@mi4.fr>
2023-08-30 15:18:37 +0200

    feat: Add doc

--- src/keycloak_saml_session/session.py
+++ src/keycloak_saml_session/session.py
@@ -4,14 +4,33 @@ import json
 from keycloak_saml_session import request
 
 def format_pem_privatekey(key):
+    """! Fonction permettant d'ajouter les entêtes
+    nécéssaire au format PEM
+
+    @param key clé privée sans les entêtes
+    @return La clé privée avec les entêtes
+    """
     return "-----BEGIN PRIVATE KEY-----\n"+key+"\n-----END PRIVATE KEY-----"
 
 
 def load_privatekey(key):
+    """! Fonction permettant de charger un clé privée
+
+    @param key clé privée au format PEM
+    @return Un objet représentant une clé privée
+    """
     return crypto.load_privatekey(crypto.FILETYPE_PEM, format_pem_privatekey(key))
 
 
 def sign_message(key, message, algo="sha256"):
+    """! Fonction permettant de signer un message
+    à partir d'une clé privée
+
+    @param key clé privée chargée
+    @param message message à signer
+    @param algo Algorithme de signature
+    @return Le message signé au format base64
+    """
     sig = crypto.sign(key, message, algo)
     return base64.b64encode(sig)
 
@@ -19,11 +38,24 @@ class SessionManager:
     SESSION_EXIST = 100
     SESSION_NOT_EXIST = 101 
     def __init__(self, host, key, reaml):
+        """! Fonction d'initialisation du SessionManager
+        
+        @param host Addresse KeyCloak
+        @param key Clé privée
+        @param reaml Reaml KeyCloak
+        """
         self.host = host
         self.key = load_privatekey(key)
         self.reaml = reaml
 
     def check_session_status(self, application, id_session):
+        """! Fonction permettant de checker le status d'un session
+        lié à l'application
+
+        @param application Nom de l'appllciation KeyCloak
+        @param id_session Numéro de la session SAML
+        @return True si le session existe, False sinon
+        """
         message = {
 	        "message": str(id_session)
         }
