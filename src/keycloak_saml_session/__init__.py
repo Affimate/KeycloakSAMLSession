3ccc21196ab6388889289e52f4e4ccea041bc0ab
Mi4BT <tb@mi4.fr>
2023-08-31 08:29:53 +0200

    chore: update version

--- src/keycloak_saml_session/__init__.py
+++ src/keycloak_saml_session/__init__.py
@@ -1,5 +1,5 @@
 from importlib import resources
 from keycloak_saml_session.session import *
 
-# Version of the realpython-reader package
-__version__ = "1.0.5"
+# Version of the keycloak_saml_session package
+__version__ = "1.0.7"
